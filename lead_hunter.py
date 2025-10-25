fa16d41143cf3665cf3f0e96f7fe254390c63f25
mattiadv-apsales <mattia.dv@andreicopy.com>
2025-10-25 15:12:07 +0200

    update

--- lead_hunter.py
+++ lead_hunter.py
@@ -36,7 +36,7 @@ OUTPUT_REPORT = "leads_report.txt"
 OUTPUT_SORTED = "leads_sorted_by_score.txt"
 OUTPUT_TOP25 = "top25.txt"
 
-SOCIAL_BLOCK_DOMAINS = {"facebook.com", "m.facebook.com", "whatsapp.com", "wa.me", "t.me", "t.co", "bit.ly"}
+SOCIAL_BLOCK_DOMAINS = {"facebook.com", "m.facebook.com", "whatsapp.com", "wa.me", "t.me", "t.co", "bit.ly", "instagram.com"}
 
 ua = UserAgent()
 HEADERS = {"User-Agent": ua.random}
